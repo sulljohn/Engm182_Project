5970533c2e6dbf6ad0b8402ab91913a857e8893b
John Sullivan <sulljohn12@gmail.com>
2020-04-27 13:44:54 -0400

    Improved import_all file

--- Import_All.R
+++ Import_All.R
@@ -6,8 +6,14 @@ source("Import_Housing_Data.R")
 # Importing crime data
 source("Import_Crime_Data.R")
 
-# Saving the workspace (it will be gitignored)
-# Once this has been made fresh,
-# it should be used for analysis
-save(df_crime, file='Data_Crime.rda')
+# EXPORTING DATA TO RDA FILES
+# Once these have been made fresh, they should be used for analysis
+
+# Saving crime data (must save dataframe, so no extra classes for Tableau)
+# Source: https://discuss.analyticsvidhya.com/t/how-to-drop-a-variable-in-r/7324
+tmp_crime <- as.data.frame(df_crime)
+save(tmp_crime, file='Data_Crime.rda')
+rm(tmp_crime)
+
+# Saving housing data
 save(df_housing, file='Data_Housing.rda')
