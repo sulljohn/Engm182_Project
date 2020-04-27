a19944eea02db5b55162ad28753b32ffc0b9064f
John Sullivan <sulljohn12@gmail.com>
2020-04-27 13:00:34 -0400

    Updated import_all to give separate files (better for Tableau)

--- Import_All.R
+++ Import_All.R
@@ -9,4 +9,5 @@ source("Import_Crime_Data.R")
 # Saving the workspace (it will be gitignored)
 # Once this has been made fresh,
 # it should be used for analysis
-save.image(file='Data_ALL.RData')
+save(df_crime, file='Data_Crime.rda')
+save(df_housing, file='Data_Housing.rda')
