75feb407d9a938eecfa3233d0149259534399bdb
viki2610 <64161717+viki2610@users.noreply.github.com>
2020-05-19 20:07:47 -0400

    Added other census variables and aggregated the data to zipcode

--- Process_Scripts/Census_Coords_to_Zipcode.R
+++ Process_Scripts/Census_Coords_to_Zipcode.R
@@ -17,9 +17,34 @@ nyc_tracts <- left_join(nyc_tracts, tract_zips, by="CensusTract")
 # Save updated nyc_tracs
 # save(nyc_tracts, file='Data_NYC_Tracts.rda')
 
-zip_population <- subset(nyc_tracts, select = c(CensusTract,zipcode,TotalPop))
+#Adding all the parameters required for regression in zip_population file
+zip_population <- subset(nyc_tracts, select = c(CensusTract,zipcode,TotalPop, Men, Women, Hispanic, White, Black, Native, Asian, IncomePerCap, Unemployment))
+
+#Converting the demographic and unemployment percentages to numbers and income per capita to income
+zip_population$Hispanic.number <- zip_population$Hispanic*zip_population$TotalPop/100
+zip_population$White.number <- zip_population$White*zip_population$TotalPop/100
+zip_population$Black.number <- zip_population$Black*zip_population$TotalPop/100
+zip_population$Native.number <- zip_population$Native*zip_population$TotalPop/100
+zip_population$Asian.number <- zip_population$Asian*zip_population$TotalPop/100
+zip_population$IncomeTot <- zip_population$IncomePerCap*zip_population$TotalPop
+#Percentage of people in the age group of 18-65 in NY is 65% source: https://www.census.gov/quickfacts/newyorkcitynewyork
+zip_population$Unemployment.number <- zip_population$Unemployment*0.65*zip_population$TotalPop/100
+
+#Aggregating the various census tract data into 1 zip code
+zip_population <- subset(zip_population, select = c(zipcode,TotalPop, Men, Women, Hispanic.number, White.number, Black.number, Native.number, Asian.number, IncomeTot, Unemployment.number))
+zip_population <- aggregate(.~zipcode, data = zip_population, FUN = sum)
+
+#Convert back the demographics and unemployment to % and IncomeTot to per capita income
+zip_population$Hispanic.number <- zip_population$Hispanic.number*100/zip_population$TotalPop
+zip_population$White.number <- zip_population$White.number*100/zip_population$TotalPop
+zip_population$Black.number <- zip_population$Black.number*100/zip_population$TotalPop
+zip_population$Native.number <- zip_population$Native.number*100/zip_population$TotalPop
+zip_population$Asian.number <- zip_population$Asian.number*100/zip_population$TotalPop
+zip_population$IncomeTot <- zip_population$IncomeTot/zip_population$TotalPop
+zip_population$Unemployment.number <- zip_population$Unemployment.number*100/(0.65*zip_population$TotalPop)
+
+#Renaming the column headers
+colnames(zip_population) <- c("Zipcode", "TotalPop", "Men", "Women", "Hispanic", "White", "Black", "Native", "Asian", "PerCapitaIncome", "Unemployed")
 
 save(zip_population, file = "Data_Zip_Population.rda")
 
-#Getting population of each zip code
-zip_population2 <- aggregate(zip_population$TotalPop, by = list(zip = zip_population$zipcode), FUN = sum)
