bfc1416d276504f824b580f1ab286ce1f2d98bae
Omkar Kshirsagar <omkar.kshirsagar@gmail.com>
2020-05-24 11:33:03 -0400

    Merged crime and housing data

--- nyc_vis_with_crimesscore.R
+++ nyc_vis_with_crimesscore.R
@@ -34,22 +34,38 @@ hist(score_by_time_and_rating$weight_log, 50)
 
 new_housing <- grouped_housing %>% 
     rename(
-         month = sale_month,
+        month_char = sale_month,
     )
+
+new_crime_score = score_by_time_and_rating %>%
+    mutate(month_char = format(as.Date(month), "%Y-%m"))%>%
+    filter(month > "2001-01-01")
+
+
+merged_housing_crime <- merge(new_housing, new_crime_score, 
+                             c("month_char", "zip_code"), all = TRUE)
+
 
 head(new_housing)
 head(score_by_time_and_rating)
 
+
 class(score_by_time_and_rating$month)
 class(new_housing$month)
 
+score_by_time_and_rating$month1 = as.Date(score_by_time_and_rating$month, format = "%Y-%d-%m")
+
+head(score_by_time_and_rating$month)
+
 class(new_housing$month)
 
+
+
 # df$ddate <- format(as.Date(df$ddate), "%d/%m/%Y")
+
 # as.Date(strDates, "%m/%d/%Y")
 
-# merged_housing_crime <- merge(new_housing, score_by_time_and_rating, 
-#                             c("month", "zip_code") )
+ )
 # 
 # # merge(observations, animals, c("size","type"))
 # 
