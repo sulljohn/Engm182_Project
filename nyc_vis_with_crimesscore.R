2fe39fdfec05d2452cb9948e1501639f066365fe
jcnoordsij <42186167+jcnoordsij@users.noreply.github.com>
2020-05-27 14:34:51 -0400

    Fixed NA values for 2015 census data in popups

--- nyc_vis_with_crimesscore.R
+++ nyc_vis_with_crimesscore.R
@@ -7,7 +7,6 @@ load(file = "Data_Score_by_Time_and_Rating.rda")
 load("grouped_housing.rda")
 #head(grouped_housing)
 
-
 summary(score_by_time_and_rating$weight)
 
 score_by_time_and_rating$weight_1000 = (score_by_time_and_rating$weight)*1000
@@ -16,44 +15,40 @@ summary(score_by_time_and_rating$weight_1000)
 
 score_by_time_and_rating$weight_log = -1*log((score_by_time_and_rating$weight), 10)
 
-summary(score_by_time_and_rating$weight_log)
-
-hist(score_by_time_and_rating$weight_log, 50)
-
-
-# library(Amelia)
-# library(pROC)
-# library("e1071")
-# library(stringi)
-# library(lubridate)
-# library(caret)
-# 
-# missmap(grouped_housing)
-# missmap(score_by_time_and_rating)
-
+score_by_time_and_rating = score_by_time_and_rating %>%
+    filter(!is.na(weight_log))
+    
+score_by_time_and_rating$weight_normalized = max(score_by_time_and_rating$weight_log) - score_by_time_and_rating$weight_log
+score_by_time_and_rating$weight_normalized = score_by_time_and_rating$weight_normalized/max(score_by_time_and_rating$weight_normalized)
 
 new_housing <- grouped_housing %>% 
     rename(
         month_char = sale_month,
     )
 
+
+unique_census = score_by_time_and_rating %>%
+    group_by(zip_code) %>%
+    summarize(PerCapitaIncome = PerCapitaIncome[1], Unemployed = Unemployed[1], TotalPop = TotalPop[1])
+    
+zip_sf = st_read("nyc_zip_code_tabulation_areas_polygons.geojson", stringsAsFactors = FALSE)
+zip_sf = merge(zip_sf, unique_census, by.x="postalcode", by.y="zip_code", all.x=TRUE)
+save(zip_sf, file="zip_polygons.rda")
+
 new_crime_score = score_by_time_and_rating %>%
     mutate(month_char = format(as.Date(month), "%Y-%m"))%>%
-    filter(month > "2001-01-01")
+    filter(month > "2002-12-31")
 
 
-merged_housing_crime <- merge(new_housing, new_crime_score, 
-                             c("month_char", "zip_code"), all = TRUE)%>%
-                    select(-c("month","weight", "weight_1000","sum_weight","Men", "Women", 
-                              "Hispanic", "White", "Black", "Native", "Asian"))
 
+merged_housing_crime <- merge(new_housing, new_crime_score, c("month_char", "zip_code"), all = TRUE) %>%
+    select(-c("month","weight", "weight_1000","sum_weight","Men", "Women", "Hispanic", "White", "Black", "Native", "Asian", "weight_log", "TotalPop", "PerCapitaIncome", "Unemployed"))
+    
 save(merged_housing_crime, file = "merged_housing_crime.rda")
 
 dim(merged_housing_crime)
 
 
-
-# 
 # head(new_housing)
 # head(score_by_time_and_rating)
 # 
