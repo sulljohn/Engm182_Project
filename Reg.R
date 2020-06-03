64f0bc4770f125f82ff37a235b6b2b088e6a1df3
John Sullivan <sulljohn12@gmail.com>
2020-06-02 20:11:56 -0400

    Updated regerssion file

--- Reg.R
+++ Reg.R
@@ -12,7 +12,7 @@ summary(df_sale_census_crime)
 df_sale_census_crime$sale_price <- as.numeric(df_sale_census_crime$sale_price)
 
 # Remove large house sale prices; source: https://stackoverflow.com/questions/25764810/delete-rows-based-on-range-of-values-in-column
-df_sale_census_crime <- df_sale_census_crime[with(df_sale_census_crime, sale_price <= 5000000), ]
+df_sale_census_crime <- df_sale_census_crime[with(df_sale_census_crime, sale_price <= 1000000), ]
 
 # Run some regressions and see what variables help the most (does crime help?)
 # You may have to do this before running the regressions:
@@ -76,53 +76,10 @@ reg_pred1 <- predict(fit1, as.matrix(x_test))
 results <- data.frame(cbind(y_test, reg_pred1))
 results$diff <- results$reg_pred1 - results$y_test
 results$diff_squared <- '^' (results$diff, 2)
-rms <- sqrt(mean(results$diff_squared))
-rms_error <- rms/mean(results$y_test)
-#RMS error is 43.73%
-# TODO: run regressions using different x variables
+# RMS = 
+rms = sqrt(mean(results$diff_squared))
+rms
 
-#Regression 2
-x2 <- df_sale_census_crime %>% select("land_square_feet", "gross_square_feet", "PerCapitaIncome", "Unemployed")
-x2$land_square_feet <- as.numeric(x2[[1]])
-x2$gross_square_feet <- as.numeric(x2[[2]])
-x2$PerCapitaIncome <- as.numeric(x2[[3]])
-x2$Unemployed <- as.numeric(x2[[4]])
-
-#Setting up factor variable for tax class
-df_sale_census_crime$tax_class_at_time_of_sale <- as.character(df_sale_census_crime$tax_class_at_time_of_sale)
-tax_class <- model.matrix( ~ tax_class_at_time_of_sale - 1, data=df_sale_census_crime )
-
-#Binding factor variables
-x2<-cbind(x2, building_class)
-x2<-cbind(x2, sale_year)
-x2<-cbind(x2, year_built)
-x2<-cbind(x2, tax_class)
-
-# Add intercept column and renaming
-x2<-cbind(x2, 1)
-colnames(x2)[dim(x2)[2]] <- "Intercept" # Source: https://www.dummies.com/programming/r/how-to-name-matrix-rows-and-columns-in-r/
-
-#x2 <- x2[!is.na(df$sale_price), ]
-# ----- Split data into training and validation sets ----- 
-# create a list of 80% of the rows in the original dataset we can use for training
-validation_index <- sort(sample(nrow(x2), nrow(x2)*.8))
-x2_test <- x2[-validation_index,]
-y2_test <- y[-validation_index]
-x2_train <- x2[validation_index,]
-y2_train <- y[validation_index]
-
-# Run regression
-fit2 <- fastLm(x_train, y2_train)
-summary(fit2)
-#R-squared value is 15.7%
-
-# Perform predictions on test set
-reg_pred2 <- predict(fit2, as.matrix(x2_test))
-
-# See the comparison
-results2 <- data.frame(cbind(y2_test, reg_pred2))
-results2$diff <- results2$reg_pred2 - results2$y_test
-results$diff_squared <- '^' (results$diff, 2)
-rms <- sqrt(mean(results$diff_squared))
-rms_error <- rms/mean(results$y_test)
-#RMS error is 43.73%
+# RMS Error = 
+rms_error = rms/mean(results$y_test)
+rms_error
